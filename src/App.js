4c7c24694e8f5c4d57acd8a840de946c8f1e8ee8
Angelos Dimitrakopoulos <angdimitrakopoulos@gmail.com>
2024-02-21 11:20:42 +0200

    commit stats

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ export default function App() {
         onDeleteItem={handleDeleteItem}
         onToggleItem={handleToggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -113,10 +113,24 @@ function Item({ item, onDeleteItem, onToggleItem }) {
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list 🚀</em>
+      </p>
+    );
+  const numItems = items.length;
+  const numItemsPacked = items.filter((item) => item.packed).length;
+  const percentPacked = Math.round((numItemsPacked / numItems) * 100);
   return (
     <footer className="stats">
-      <em>💼 You have X items on your list, and you already packed X (X%)</em>
+      <em>
+        {percentPacked === 100
+          ? "You got everything ready to go ✈"
+          : `💼 You have ${numItems} items on your list, and you already packed
+        ${numItemsPacked} (${percentPacked}%)`}
+      </em>
     </footer>
   );
 }
